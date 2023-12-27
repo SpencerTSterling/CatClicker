f2e846acfec12690c2349e709657fa3dbc6927c6
Spencer Sterling <spencer.sterling2001@gmail.com>
2023-12-26 23:34:12 -0800

    Added clickClicker function to index.js()

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
-import './buildings'
 
 let cat = document.querySelector('#cat');
 let totalKitties = document.querySelector('#total-kitties');
 let totalKittiesNum = parseFloat(totalKitties.innerHTML);
 
+let cpsElement = document.querySelector('#cps');
+let cps = parseFloat(cpsElement.innerHTML);
+
+let clickerElement = document.querySelector('.building');
+
 function clickCat() {
+    console.log("Clicked the cat!");
     totalKittiesNum++;
     updateTotalKitties();
 }
@@ -15,3 +20,14 @@ function updateTotalKitties() {
 
 // Add a click event listener to the cat element
 cat.addEventListener('click', clickCat);
+
+//// Building logic
+
+function clickClicker() {
+    alert('You clicked the clicker!');
+    clicker.buyBuilding(totalKittiesNum, cps);
+    updateTotalKitties();
+    updateCPS(cps);
+}
+
+clickerElement.addEventListener('click', clickClicker);
