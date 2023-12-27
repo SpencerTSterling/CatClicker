60059096018742f0a67b1c94b287441ef85298e9
Spencer Sterling <spencer.sterling2001@gmail.com>
2023-12-26 21:19:54 -0800

    Cat responds to click

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-let totalKitties = 0
+let cat = document.querySelector('#cat')
+
+// Add a click event listener to the cat element
+cat.addEventListener('click', function () {
+    alert('You clicked the cat!');
+});
