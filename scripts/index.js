9f36850b4d9b2db31cbfa591708889b623ba96d3
Spencer Sterling <spencer.sterling2001@gmail.com>
2023-12-27 22:39:39 -0800

    New building div added to webpage

--- scripts/index.js
+++ scripts/index.js
@@ -14,19 +14,6 @@ let clickerBox= document.querySelector('.building');
 
 let cheatBtn = document.querySelector('#cheat');
 
-//test function
-function showBuildingsUnlocked(buildingList) {
-    console.log('Buildings:');
-  
-    for (const building of buildingList) {
-      if (building.available) {
-        console.log(`${building.name} is available`);
-      } else {
-        console.log(`${building.name} is locked`);
-      }
-    }
-  }
-
 
 function clickCat() {
     console.log("Clicked the cat!");
@@ -55,7 +42,6 @@ function autoAddCPS(cps) {
         updateTotalKitties(); // Update the total kitties displayed on the page
     }, 1000); // CPS is per second, so the interval is set to 1000ms (1 second)
 }
-
 // Call the function to start auto-adding kitties based on CPS
 autoAddCPS(cps);
 
@@ -110,10 +96,14 @@ function buyBuilding(Building) {
             // unlock the next building in the list
             const nextBuilding = buildings.buildingList[currentIndex + 1];
             nextBuilding.unlock();
+            console.log(`Building Unlocked: ${nextBuilding.name}`);
+
+            // Create and append the HTML for the new building
+            const buildingElement = createBuildingElement(nextBuilding);
+            appendBuildingToContainer(buildingElement);
+            console.log(`Building Added: ${nextBuilding.name}`);
         }
 
-        //(test function) Shows which buildings are locked/unlocked
-        //showBuildingsUnlocked(buildings.buildingList);
         
    } else {
         console.log(`Not enough kitties to upgrade ${Building.name}!`);
@@ -121,6 +111,59 @@ function buyBuilding(Building) {
 }
 
 clickerBox.addEventListener('click', () => buyBuilding(buildings.clicker));
+
+
+function createBuildingElement(building){
+    const buildingElement = document.createElement('div');
+    buildingElement.classList.add('building')
+
+
+    // Add building icon
+    const iconElement = document.createElement('div');
+    iconElement.classList.add('building-icon');
+    const iconImg = document.createElement('img');
+    iconImg.src = `./assets/${building.name.toLowerCase().replace(' ', '')}.png`;
+    iconElement.appendChild(iconImg);
+
+    // Add building info
+    const infoElement = document.createElement('div');
+    infoElement.classList.add('building-info');
+    const nameElement = document.createElement('h4');
+    nameElement.classList.add('building-name');
+    nameElement.textContent = building.name;
+    const costInfoElement = document.createElement('div');
+    costInfoElement.classList.add('building-cost-info');
+    const costTextElement = document.createElement('p');
+    costTextElement.textContent = `Cost: ${building.cost}`;
+    const currencyImgElement = document.createElement('img');
+    currencyImgElement.src = './assets/catcoin.png';
+    costInfoElement.appendChild(costTextElement);
+    costInfoElement.appendChild(currencyImgElement);
+
+    // Add building level info
+    const levelInfoElement = document.createElement('div');
+    levelInfoElement.classList.add('building-level-info');
+    const levelTextElement = document.createElement('p');
+    levelTextElement.textContent = `Lvl: ${building.level}`;
+    levelInfoElement.appendChild(levelTextElement);
+
+    // Append elements
+    infoElement.appendChild(nameElement);
+    infoElement.appendChild(costInfoElement);
+    buildingElement.appendChild(iconElement);
+    buildingElement.appendChild(infoElement);
+    buildingElement.appendChild(levelInfoElement);
+
+    return buildingElement;
+
+}
+
+function appendBuildingToContainer(buildingElement) {
+    const buildingContainer = document.querySelector('.building-container');
+    buildingContainer.appendChild(buildingElement);
+  }
+
+
 
 
 // ###### Cheats ##### //
